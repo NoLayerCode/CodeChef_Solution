976c3434752004f2305999cf3eb902e7135055c1
NoLayerCode <vyelhekar@gmail.com>
2018-09-04 00:00:32 +0530

    First CodeChef

---
+++ LifeEverything .cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int ip;
+	while(1){
+		cin>>ip;
+		if (ip == 42){
+			break;
+		}
+		cout<<ip<<endl;
+	}
+	return 0;
+}
